6e1e159f782ff6ec9c26c59380f5919a0103b075
ckcks12 <wyoducc@gmail.com>
2017-04-13 22:18:24 +0900

    add KwixDatabase

---
+++ KwixDatabase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xTwitter_collector
+{
+    public class KwixDatabase
+    {
+    }
+}
