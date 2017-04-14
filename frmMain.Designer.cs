19234993d6ae9ff19de0c6719dc25526bc2b497f
ckcks12 <wyoducc@gmail.com>
2017-04-14 12:53:08 +0900

    form gui being grouped

--- frmMain.Designer.cs
+++ frmMain.Designer.cs
@@ -28,14 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.btnDbConnectionTest = new System.Windows.Forms.Button();
             this.tbDbPassword = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lvApiToken = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tbApiTokenName = new System.Windows.Forms.TextBox();
+            this.tbApiTokenConsumerKey = new System.Windows.Forms.TextBox();
+            this.tbApiTokenConsumerSecret = new System.Windows.Forms.TextBox();
+            this.tbApiTokenAccessToken = new System.Windows.Forms.TextBox();
+            this.tbApiTokenAccessTokenSecret = new System.Windows.Forms.TextBox();
+            this.btnApiTokenAdd = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabDbConfig = new System.Windows.Forms.TabPage();
+            this.tabApiToken = new System.Windows.Forms.TabPage();
+            this.tabCollectTest = new System.Windows.Forms.TabPage();
+            this.tabQueue = new System.Windows.Forms.TabPage();
+            this.tabStatus = new System.Windows.Forms.TabPage();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabDbConfig.SuspendLayout();
+            this.tabApiToken.SuspendLayout();
+            this.tabCollectTest.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -43,7 +65,7 @@
             this.groupBox1.Controls.Add(this.btnDbConnectionTest);
             this.groupBox1.Controls.Add(this.tbDbPassword);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Location = new System.Drawing.Point(278, 243);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(203, 79);
             this.groupBox1.TabIndex = 0;
@@ -79,12 +101,190 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "password";
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnApiTokenAdd);
+            this.groupBox2.Controls.Add(this.tbApiTokenAccessTokenSecret);
+            this.groupBox2.Controls.Add(this.tbApiTokenAccessToken);
+            this.groupBox2.Controls.Add(this.tbApiTokenConsumerSecret);
+            this.groupBox2.Controls.Add(this.tbApiTokenConsumerKey);
+            this.groupBox2.Controls.Add(this.tbApiTokenName);
+            this.groupBox2.Controls.Add(this.lvApiToken);
+            this.groupBox2.Location = new System.Drawing.Point(6, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(715, 357);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Twitter API Token";
+            // 
+            // lvApiToken
+            // 
+            this.lvApiToken.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.lvApiToken.FullRowSelect = true;
+            this.lvApiToken.GridLines = true;
+            this.lvApiToken.Location = new System.Drawing.Point(6, 20);
+            this.lvApiToken.Name = "lvApiToken";
+            this.lvApiToken.Size = new System.Drawing.Size(703, 293);
+            this.lvApiToken.TabIndex = 0;
+            this.lvApiToken.UseCompatibleStateImageBehavior = false;
+            this.lvApiToken.View = System.Windows.Forms.View.Details;
+            this.lvApiToken.SelectedIndexChanged += new System.EventHandler(this.lvApiToken_SelectedIndexChanged);
+            this.lvApiToken.ClientSizeChanged += new System.EventHandler(this.lvApiToken_ClientSizeChanged);
+            this.lvApiToken.Click += new System.EventHandler(this.lvApiToken_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "name";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "ConsumerKey";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "ConsumerSecret";
+            this.columnHeader3.Width = 152;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "AccessToken";
+            this.columnHeader4.Width = 169;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "AccessTokenSecret";
+            this.columnHeader5.Width = 196;
+            // 
+            // tbApiTokenName
+            // 
+            this.tbApiTokenName.Location = new System.Drawing.Point(6, 319);
+            this.tbApiTokenName.Name = "tbApiTokenName";
+            this.tbApiTokenName.Size = new System.Drawing.Size(61, 21);
+            this.tbApiTokenName.TabIndex = 1;
+            // 
+            // tbApiTokenConsumerKey
+            // 
+            this.tbApiTokenConsumerKey.Location = new System.Drawing.Point(73, 319);
+            this.tbApiTokenConsumerKey.Name = "tbApiTokenConsumerKey";
+            this.tbApiTokenConsumerKey.Size = new System.Drawing.Size(106, 21);
+            this.tbApiTokenConsumerKey.TabIndex = 2;
+            // 
+            // tbApiTokenConsumerSecret
+            // 
+            this.tbApiTokenConsumerSecret.Location = new System.Drawing.Point(201, 319);
+            this.tbApiTokenConsumerSecret.Name = "tbApiTokenConsumerSecret";
+            this.tbApiTokenConsumerSecret.Size = new System.Drawing.Size(106, 21);
+            this.tbApiTokenConsumerSecret.TabIndex = 3;
+            // 
+            // tbApiTokenAccessToken
+            // 
+            this.tbApiTokenAccessToken.Location = new System.Drawing.Point(313, 319);
+            this.tbApiTokenAccessToken.Name = "tbApiTokenAccessToken";
+            this.tbApiTokenAccessToken.Size = new System.Drawing.Size(106, 21);
+            this.tbApiTokenAccessToken.TabIndex = 4;
+            // 
+            // tbApiTokenAccessTokenSecret
+            // 
+            this.tbApiTokenAccessTokenSecret.Location = new System.Drawing.Point(450, 319);
+            this.tbApiTokenAccessTokenSecret.Name = "tbApiTokenAccessTokenSecret";
+            this.tbApiTokenAccessTokenSecret.Size = new System.Drawing.Size(106, 21);
+            this.tbApiTokenAccessTokenSecret.TabIndex = 5;
+            // 
+            // btnApiTokenAdd
+            // 
+            this.btnApiTokenAdd.Location = new System.Drawing.Point(561, 319);
+            this.btnApiTokenAdd.Name = "btnApiTokenAdd";
+            this.btnApiTokenAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnApiTokenAdd.TabIndex = 6;
+            this.btnApiTokenAdd.Text = "add";
+            this.btnApiTokenAdd.UseVisualStyleBackColor = true;
+            this.btnApiTokenAdd.Click += new System.EventHandler(this.btnApiTokenAdd_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabDbConfig);
+            this.tabControl1.Controls.Add(this.tabApiToken);
+            this.tabControl1.Controls.Add(this.tabCollectTest);
+            this.tabControl1.Controls.Add(this.tabQueue);
+            this.tabControl1.Controls.Add(this.tabStatus);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(735, 607);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabDbConfig
+            // 
+            this.tabDbConfig.Controls.Add(this.groupBox1);
+            this.tabDbConfig.Location = new System.Drawing.Point(4, 22);
+            this.tabDbConfig.Name = "tabDbConfig";
+            this.tabDbConfig.Padding = new System.Windows.Forms.Padding(3);
+            this.tabDbConfig.Size = new System.Drawing.Size(727, 581);
+            this.tabDbConfig.TabIndex = 0;
+            this.tabDbConfig.Text = "DB Connection";
+            this.tabDbConfig.UseVisualStyleBackColor = true;
+            // 
+            // tabApiToken
+            // 
+            this.tabApiToken.Controls.Add(this.groupBox2);
+            this.tabApiToken.Location = new System.Drawing.Point(4, 22);
+            this.tabApiToken.Name = "tabApiToken";
+            this.tabApiToken.Padding = new System.Windows.Forms.Padding(3);
+            this.tabApiToken.Size = new System.Drawing.Size(727, 581);
+            this.tabApiToken.TabIndex = 1;
+            this.tabApiToken.Text = "Api Token";
+            this.tabApiToken.UseVisualStyleBackColor = true;
+            // 
+            // tabCollectTest
+            // 
+            this.tabCollectTest.Controls.Add(this.groupBox3);
+            this.tabCollectTest.Location = new System.Drawing.Point(4, 22);
+            this.tabCollectTest.Name = "tabCollectTest";
+            this.tabCollectTest.Padding = new System.Windows.Forms.Padding(3);
+            this.tabCollectTest.Size = new System.Drawing.Size(727, 581);
+            this.tabCollectTest.TabIndex = 2;
+            this.tabCollectTest.Text = "Collect Test";
+            this.tabCollectTest.UseVisualStyleBackColor = true;
+            // 
+            // tabQueue
+            // 
+            this.tabQueue.Location = new System.Drawing.Point(4, 22);
+            this.tabQueue.Name = "tabQueue";
+            this.tabQueue.Size = new System.Drawing.Size(634, 581);
+            this.tabQueue.TabIndex = 3;
+            this.tabQueue.Text = "Queue";
+            this.tabQueue.UseVisualStyleBackColor = true;
+            // 
+            // tabStatus
+            // 
+            this.tabStatus.Location = new System.Drawing.Point(4, 22);
+            this.tabStatus.Name = "tabStatus";
+            this.tabStatus.Size = new System.Drawing.Size(634, 581);
+            this.tabStatus.TabIndex = 4;
+            this.tabStatus.Text = "Status";
+            this.tabStatus.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(6, 6);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 531);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "timeline ";
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(666, 576);
-            this.Controls.Add(this.groupBox1);
+            this.ClientSize = new System.Drawing.Size(759, 631);
+            this.Controls.Add(this.tabControl1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmMain";
@@ -93,7 +293,12 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabDbConfig.ResumeLayout(false);
+            this.tabApiToken.ResumeLayout(false);
+            this.tabCollectTest.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -104,7 +309,26 @@
         private System.Windows.Forms.Button btnDbConnectionTest;
         private System.Windows.Forms.TextBox tbDbPassword;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView lvApiToken;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.Button btnApiTokenAdd;
+        private System.Windows.Forms.TextBox tbApiTokenAccessTokenSecret;
+        private System.Windows.Forms.TextBox tbApiTokenAccessToken;
+        private System.Windows.Forms.TextBox tbApiTokenConsumerSecret;
+        private System.Windows.Forms.TextBox tbApiTokenConsumerKey;
+        private System.Windows.Forms.TextBox tbApiTokenName;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabDbConfig;
+        private System.Windows.Forms.TabPage tabApiToken;
+        private System.Windows.Forms.TabPage tabCollectTest;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TabPage tabQueue;
+        private System.Windows.Forms.TabPage tabStatus;
     }
 }
 
--- frmMain.cs
+++ frmMain.cs
@@ -39,6 +39,8 @@ namespace xTwitter_collector
                 // 성공했으면 버튼하고 텍스트박스 비활성화
                 tbDbPassword.Enabled = false;
                 btnDbConnectionTest.Enabled = false;
+                // api token listview에 gui update
+                SyncApiTokenListView();
             }
             catch (Exception ex)
             {
@@ -46,10 +48,61 @@ namespace xTwitter_collector
             }
         }
 
+        public void SyncApiTokenListView()
+        {
+            lvApiToken.Items.Clear();
+            foreach (ApiToken token in kd.ReadApiToken())
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = token.name;
+                item.SubItems.Add(token.consumer_key);
+                item.SubItems.Add(token.consumer_secret);
+                item.SubItems.Add(token.access_token);
+                item.SubItems.Add(token.access_token_secret);
+                lvApiToken.Items.Add(item);
+            }
+        }
+
         private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
         {
             // 각 폼에 binding 되어있는 것들 .. 저장함... 
             Properties.Settings.Default.Save();
+        }
+
+        private void btnApiTokenAdd_Click(object sender, EventArgs e)
+        {
+            kd.CreateApiToken(new ApiToken()
+            {
+                id = kd.ReadApiToken().Count(),
+                name = tbApiTokenName.Text,
+                consumer_key = tbApiTokenConsumerKey.Text,
+                consumer_secret = tbApiTokenConsumerSecret.Text,
+                access_token = tbApiTokenAccessToken.Text,
+                access_token_secret = tbApiTokenAccessTokenSecret.Text
+            });
+
+            SyncApiTokenListView();
+        }
+
+        private void lvApiToken_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvApiToken_ClientSizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvApiToken_Click(object sender, EventArgs e)
+        {
+            // listview 클릭하면 해당 내용이 textbox들에 뜨게... 나중에 누가 구현좀^^
+            ListViewItem item = lvApiToken.SelectedItems[0];
+            if( item == null )
+            {
+                return;
+            }
+            
         }
     }
 }
